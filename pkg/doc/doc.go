602c6cab7d294ee931b180165083401ef32c6ccd
Manuel Odendahl <wesen@ruinwesen.com>
2026-06-10 14:08:45 -0400

    Preserve embedded docs filesystem for help export

--- pkg/doc/doc.go
+++ pkg/doc/doc.go
@@ -1,3 +1,8 @@
+// Package doc contains embedded Markdown help entries for downstream tools.
+//
+// Applications that already depend on Glazed can load the documents through
+// AddDocToHelpSystem. Non-Glazed consumers can keep mounting FS directly as a
+// regular fs.FS.
 package doc
 
 import (
@@ -6,9 +11,16 @@ import (
 	"github.com/go-go-golems/glazed/pkg/help"
 )
 
-//go:embed topics/*
-var docFS embed.FS
+// FS contains the embedded logcopter help documents.
+//
+// Glazed and other downstream tools can mount this filesystem directly instead
+// of locating a checkout of the logcopter repository at runtime.
+//
+//go:embed topics/*.md tutorials/*.md
+var FS embed.FS
 
+// AddDocToHelpSystem loads the embedded logcopter help documents into a Glazed
+// help system.
 func AddDocToHelpSystem(helpSystem *help.HelpSystem) error {
-	return helpSystem.LoadSectionsFromFS(docFS, ".")
+	return helpSystem.LoadSectionsFromFS(FS, ".")
 }
